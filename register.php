6ecb18851f56466a6c58dbf8f1ac3e1b28351d52
informer735 <informer735@gmail.com>
2019-05-14 09:38:49 +0300

    6 lessons
    реализация регистрации

---
+++ register.php
@@ -0,0 +1,27 @@
+<?php
+
+foreach($_POST as $input) {
+    if(empty($input)) {
+        include 'errors.php';
+        exit;
+    }
+}
+
+$pdo = new PDO('mysql:host=localhost;dbname=users', 'root', '');
+$sql = 'SELECT id from users WHERE email=:email';
+$statement = $pdo->prepare($sql);
+$statement->execute(([':email' => $_POST['email']]));
+$user = $statement->fetchColumn();
+
+if($user) {
+    $errorMessage = 'Пользователь с таким e-mail уже существует';
+    include 'errors.php';
+    exit;
+}
+
+ $sql = 'INSERT INTO users (name, email, password) VALUES (:name, :email, :password)';
+ $statement = $pdo->prepare($sql);
+ $_POST['password'] = md5($_POST['password']);
+ $statement->execute($_POST);
+
+header('Location: /login-form.php'); exit;
